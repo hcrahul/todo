4e435e0793c0ceb183ed45be02c3aa889884a677
devamanin <amansingh1032000@gmail.com>
2022-10-31 01:04:32 +0530

    added todo add button

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import TodoHeader from './components/header/TodoHeader'
 import TodoContainer from './components/TodoContainer/TodoContainer'
-// import Todo from './components/Todo/Todo'
-
-import './App.css'
 import Todo from './components/Todo/Todo'
+import TodoAdd from './components/TodoAdd/TodoAdd'
+import './App.css'
+// import Todo from './components/Todo/Todo'
 
 export default function App() {
   return (
     <div className='main-container'>
       <TodoHeader/>
-      <TodoContainer/>
+      <TodoContainer>
+        <Todo/>
+        <Todo/>
+        <Todo/>
+      </TodoContainer>
+      <TodoAdd/>
     </div>
   )
 }
---
+++ todo/src/components/TodoAdd/TodoAdd.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {IoIosAddCircleOutline} from 'react-icons/io'
+
+export default function TodoAdd() {
+  return (
+    <div className='todo-add-button'>
+        {/* <IoIosAddCircleOutline size={'30px'} color="#0c10ff"/> */}
+        <IoIosAddCircleOutline size={'30px'} color="#fff"/>
+    </div>
+  )
+}
