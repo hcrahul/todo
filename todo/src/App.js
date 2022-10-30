fc139ddac2b4326d13fd0f08d2f230e56e0235d3
devamanin <amansingh1032000@gmail.com>
2022-10-30 17:31:54 +0530

    Added Todo item'

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import TodoHeader from './components/header/TodoHeader'
 import TodoContainer from './components/TodoContainer/TodoContainer'
+// import Todo from './components/Todo/Todo'
 
 import './App.css'
+import Todo from './components/Todo/Todo'
 
 export default function App() {
   return (
--- todo/src/components/Todo/Todo.jsx
+++ todo/src/components/Todo/Todo.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Todo() {
   return (
     <div className='todo-item'>
-        
+        <div className="title">
+          My Startup ideas
+        </div>
     </div>
   )
 }
