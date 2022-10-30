bf8d8b5fe60d985b8beef75f4b4ffc8db2bad50a
devamanin <amansingh1032000@gmail.com>
2022-10-30 17:59:00 +0530

    Added done button in todo item

--- todo/src/components/Todo/Todo.jsx
+++ todo/src/components/Todo/Todo.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {IoMdDoneAll} from 'react-icons/io'
 
 export default function Todo() {
   return (
     <div className='todo-item'>
         <div className="title">
           My Startup ideas
+        </div>
+        <div className="done-bt-container">
+          <IoMdDoneAll color='#1D1CE5'/>
         </div>
     </div>
   )
